00f813914dc5cde703792fcc463e14e779cd4a4d
Makeev Alexey <makeevhome@mail.ru>
2023-01-19 23:59:23 +0300

    Dz2

---
+++ src/Dz2/Dog.java
@@ -0,0 +1,13 @@
+package Dz2;
+
+public class Dog extends PetAnimal{
+
+  public Dog(String nickName, int age) {
+    super(nickName, age);
+  }
+
+  @Override
+  public void respond() {
+    System.out.println("Woof-woof");
+  }
+}
